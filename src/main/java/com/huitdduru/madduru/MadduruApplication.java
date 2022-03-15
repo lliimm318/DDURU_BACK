5c1e15894e203b5315a7f53001b717c3131a6a2f
임서영 <sylim0318@naver.com>
2022-03-15 20:00:14 +0900

    ADD

---
+++ src/main/java/com/huitdduru/madduru/MadduruApplication.java
@@ -0,0 +1,13 @@
+package com.huitdduru.madduru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadduruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadduruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huitdduru/madduru/MadduruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huitdduru.madduru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadduruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

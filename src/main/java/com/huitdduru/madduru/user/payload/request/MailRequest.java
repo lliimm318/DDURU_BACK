de0c457b2d927c2865b2a1f25bd39bf43fbd5fd3
임서영 <sylim0318@naver.com>
2022-03-21 10:43:38 +0900

    [ADD] mail request

---
+++ src/main/java/com/huitdduru/madduru/user/payload/request/MailRequest.java
@@ -0,0 +1,12 @@
+package com.huitdduru.madduru.user.payload.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class MailRequest {
+
+    private final String email;
+
+}

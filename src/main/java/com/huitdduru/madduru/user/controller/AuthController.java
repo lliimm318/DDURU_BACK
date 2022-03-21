5fd72c1f88f6da231bbdbbd80b88217ffcf00dcd
임서영 <sylim0318@naver.com>
2022-03-21 10:43:01 +0900

    [ADD] auth controller

---
+++ src/main/java/com/huitdduru/madduru/user/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.huitdduru.madduru.user.controller;
+
+import com.huitdduru.madduru.user.payload.request.AuthRequest;
+import com.huitdduru.madduru.user.payload.request.RegisterRequest;
+import com.huitdduru.madduru.user.payload.response.TokenResponse;
+import com.huitdduru.madduru.user.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    void register(@RequestBody RegisterRequest registerRequest) throws IOException {
+        authService.register(registerRequest);
+    }
+
+    @PostMapping("/auth")
+    TokenResponse signIn(@RequestBody AuthRequest authRequest) {
+        return authService.auth(authRequest);
+    }
+
+    @PutMapping("/auth")
+    TokenResponse refresh(@RequestHeader("refresh-token") String refreshToken) {
+        return authService.refreshToken(refreshToken);
+    }
+
+}

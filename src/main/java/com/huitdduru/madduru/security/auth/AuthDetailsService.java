e64c73e12e6f8d3f46a0f476d5fa1d6b3f40ed80
임서영 <sylim0318@naver.com>
2022-03-21 10:42:03 +0900

    [ADD] auth details service

---
+++ src/main/java/com/huitdduru/madduru/security/auth/AuthDetailsService.java
@@ -0,0 +1,22 @@
+package com.huitdduru.madduru.security.auth;
+
+import com.huitdduru.madduru.exception.exceptions.UserNotFoundException;
+import com.huitdduru.madduru.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public AuthDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .map(AuthDetails::new)
+                .orElseThrow(UserNotFoundException::new);
+    }
+}

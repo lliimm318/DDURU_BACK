ed16f8f041f3b037add16a5ea7b19697bd208bdc
임서영 <sylim0318@naver.com>
2022-03-28 10:13:49 +0900

    [ADD] refresh token repository

--- src/main/java/com/huitdduru/madduru/redis/RefreshTokenRepository.java
+++ src/main/java/com/huitdduru/madduru/redis/RefreshTokenRepository.java
@@ -1,6 +1,5 @@
-package com.huitdduru.madduru.user.domain.repository;
+package com.huitdduru.madduru.redis;
 
-import com.huitdduru.madduru.redis.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 

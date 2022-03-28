5a11d44db71dc4d697d1307d5c04dd4ebd5189e0
임서영 <sylim0318@naver.com>
2022-03-28 10:11:50 +0900

    [UPDATE] email service

--- src/main/java/com/huitdduru/madduru/user/service/EmailService.java
+++ src/main/java/com/huitdduru/madduru/user/service/EmailService.java
@@ -1,9 +1,12 @@
 package com.huitdduru.madduru.user.service;
 
 import com.huitdduru.madduru.user.payload.request.MailRequest;
+import com.huitdduru.madduru.user.payload.request.RandomRequest;
 
 public interface EmailService {
 
     void sendMail(MailRequest mailRequest);
+
+    void randomCode(RandomRequest randomRequest);
 
 }
--- src/main/java/com/huitdduru/madduru/user/service/EmailServiceImpl.java
+++ src/main/java/com/huitdduru/madduru/user/service/EmailServiceImpl.java
@@ -1,12 +1,17 @@
 package com.huitdduru.madduru.user.service;
 
-import com.huitdduru.madduru.user.domain.User;
-import com.huitdduru.madduru.user.domain.repository.UserRepository;
+import com.huitdduru.madduru.exception.exceptions.MailSendException;
+import com.huitdduru.madduru.email.entity.RandomCode;
+import com.huitdduru.madduru.email.repository.RandomCodeRepository;
+import com.huitdduru.madduru.exception.exceptions.VerifyNumNotFoundException;
 import com.huitdduru.madduru.user.payload.request.MailRequest;
+import com.huitdduru.madduru.user.payload.request.RandomRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Random;
 
@@ -14,19 +19,54 @@ import java.util.Random;
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
-    private JavaMailSender javaMailSender;
+    private final static Random RANDOM = new Random();
+    private final JavaMailSender javaMailSender;
 
+    private final RandomCodeRepository randomCodeRepository;
+
+    @Transactional
     @Override
     public void sendMail(MailRequest mailRequest) {
-        SimpleMailMessage message = new SimpleMailMessage();
+       String randomCode = generateRandomCode();
 
-        Random random = new Random();
-        String randomCode = String.valueOf(random.nextInt());
+       try {
+           final MimeMessagePreparator preparator = mimeMessage -> {
+               final MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+               helper.setFrom("huitddu@gmail.com");
+               helper.setTo(mailRequest.getEmail());
+               helper.setSubject("휘뚜루마뚜루 인종 번호는 " + randomCode + "입니다");
+               helper.setText("휘뚜루마뚜루");
+           };
+           javaMailSender.send(preparator);
 
-        message.setTo(mailRequest.getEmail());
-        message.setSubject("휘뚜루마뚜루 인증 코드입니다.");
-        message.setText(randomCode);
-        javaMailSender.send((message));
+           randomCodeRepository.findByEmail(mailRequest.getEmail())
+                   .ifPresent(randomCodeRepository::delete);
+
+           RandomCode random = RandomCode.builder()
+                   .email(mailRequest.getEmail())
+                   .randomCode(randomCode)
+                   .build();
+
+           randomCodeRepository.save(random);
+       } catch (Exception e) {
+           e.printStackTrace();
+           throw new MailSendException();
+       }
+    }
+
+    private String generateRandomCode() {
+        RANDOM.setSeed(System.currentTimeMillis());
+
+        return Integer.toString(RANDOM.nextInt(1000000) % 1000000);
+    }
+
+    @Override
+    public void randomCode(RandomRequest randomRequest) {
+        randomCodeRepository.findByEmail(randomRequest.getEmail())
+                .filter(randomCode -> randomCode.getRandomCode().equals(randomRequest.getCode()))
+                .map(RandomCode::isVerifiedTrue)
+                .map(randomCodeRepository::save)
+                .orElseThrow(VerifyNumNotFoundException::new);
     }
 
 }

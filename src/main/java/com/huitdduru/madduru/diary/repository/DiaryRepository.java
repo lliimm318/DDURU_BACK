726054d8e00de2564b48d0eca522c2a7dff24db4
kwakdony <kwakdh25@gmail.com>
2022-04-21 10:20:36 +0900

    [ADD] Repository 메소드 추가

--- src/main/java/com/huitdduru/madduru/diary/repository/DiaryRepository.java
+++ src/main/java/com/huitdduru/madduru/diary/repository/DiaryRepository.java
@@ -1,8 +1,16 @@
 package com.huitdduru.madduru.diary.repository;
 
 import com.huitdduru.madduru.diary.entity.Diary;
+import com.huitdduru.madduru.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DiaryRepository extends JpaRepository<Diary, Integer> {
 
+    @Query(value = "select d from Diary d where (d.user1 = ?1 and d.user2 != ?1) or (d.user2 = ?1 and d.user1 != ?1)")
+    List<Diary> findByUser1OrUser2(User user);
+
+    Diary findByUser1AndUser2(User user1, User user2);
 }

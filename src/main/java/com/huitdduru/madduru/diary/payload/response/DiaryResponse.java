52ec6f5a381b779de5054ed04412dd355164e395
kwakdony <kwakdh25@gmail.com>
2022-04-21 10:19:59 +0900

    [ADD] Dto

---
+++ src/main/java/com/huitdduru/madduru/diary/payload/response/DiaryResponse.java
@@ -0,0 +1,22 @@
+package com.huitdduru.madduru.diary.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class DiaryResponse {
+    private final Integer diaryId;
+
+    private final String loginUserImg;
+
+    private final String mateImg;
+
+    private final Boolean isMyTurn;
+
+    private final String mateName;
+
+    private final Long hoursAgo;
+}

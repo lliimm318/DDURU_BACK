873a3b8ef43c1b060ca43710fbb3a016c5a3ae48
seo young <sylim0318@naver.com>
2022-04-25 19:29:44 +0900

    🔌 (#9) - orm 추가

--- src/main/java/com/huitdduru/madduru/diary/repository/DiaryDetailRepository.java
+++ src/main/java/com/huitdduru/madduru/diary/repository/DiaryDetailRepository.java
@@ -1,7 +1,15 @@
 package com.huitdduru.madduru.diary.repository;
 
+import com.huitdduru.madduru.diary.entity.Diary;
 import com.huitdduru.madduru.diary.entity.DiaryDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface DiaryDetailRepository extends JpaRepository<DiaryDetail, Integer> {
+
+    List<DiaryDetail> findByDiaryOrderByCreatedAt(Diary diary);
+
 }

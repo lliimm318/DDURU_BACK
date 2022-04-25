1b789bb87a610d02fe05fdc791278aa6422e5500
seo young <sylim0318@naver.com>
2022-04-25 19:30:00 +0900

    🔌 (#9) - orm 추강

--- src/main/java/com/huitdduru/madduru/diary/repository/DiaryRepository.java
+++ src/main/java/com/huitdduru/madduru/diary/repository/DiaryRepository.java
@@ -1,7 +1,18 @@
 package com.huitdduru.madduru.diary.repository;
 
 import com.huitdduru.madduru.diary.entity.Diary;
+import com.huitdduru.madduru.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface DiaryRepository extends JpaRepository<Diary, Integer> {
+
+    List<Diary> findByUser1OrUser2(User user);
+
+    Optional<Diary> findByUser1OrUser2AndRelationContinuesIsTrue(User user);
+
 }

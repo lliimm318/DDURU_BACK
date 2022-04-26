7a4413f2c0939b984b5c4a8235f3f3b8d5b793f4
seo young <sylim0318@naver.com>
2022-04-26 22:49:11 +0900

    😎 (#9) - setter 추가

--- src/main/java/com/huitdduru/madduru/diary/payload/response/DiaryDetailResponse.java
+++ src/main/java/com/huitdduru/madduru/diary/payload/response/DiaryDetailResponse.java
@@ -1,11 +1,10 @@
 package com.huitdduru.madduru.diary.payload.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.huitdduru.madduru.diary.entity.DiaryDetail;
+import lombok.*;
 
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor

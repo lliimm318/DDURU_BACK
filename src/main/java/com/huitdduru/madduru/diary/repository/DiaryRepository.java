1566445bc4a603d28cf7d7fec3065d3491404cbe
seo young <sylim0318@naver.com>
2022-04-26 22:50:40 +0900

    😎 (#9) - 쿼리문 추가

--- src/main/java/com/huitdduru/madduru/diary/repository/DiaryRepository.java
+++ src/main/java/com/huitdduru/madduru/diary/repository/DiaryRepository.java
@@ -3,6 +3,7 @@ package com.huitdduru.madduru.diary.repository;
 import com.huitdduru.madduru.diary.entity.Diary;
 import com.huitdduru.madduru.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,8 +12,10 @@ import java.util.Optional;
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Integer> {
 
+    @Query(value = "select d from Diary d where (d.user1 = ?1 and d.user2 != ?1) or (d.user2 = ?1 and d.user1 != ?1)")
     List<Diary> findByUser1OrUser2(User user);
 
+    @Query(value = "select d from Diary d where (d.user1 = ?1 and d.user2 != ?1) or (d.user2 = ?1 and d.user1 != ?1)")
     Optional<Diary> findByUser1OrUser2AndRelationContinuesIsTrue(User user);
 
 }

ec3646555157a9430cbf9d6c498c14d4298907f2
임서영 <sylim0318@naver.com>
2022-05-10 14:11:44 +0900

    [FIX] import문 삭제

--- src/main/java/com/huitdduru/madduru/diary/payload/response/DiaryDetailResponse.java
+++ src/main/java/com/huitdduru/madduru/diary/payload/response/DiaryDetailResponse.java
@@ -1,10 +1,8 @@
 package com.huitdduru.madduru.diary.payload.response;
 
-import com.huitdduru.madduru.diary.entity.DiaryDetail;
 import lombok.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter

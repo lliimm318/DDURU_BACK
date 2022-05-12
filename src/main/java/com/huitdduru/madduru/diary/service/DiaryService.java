e470f635f741a5c58e603649d45a0b904e1754e3
임서영 <sylim0318@naver.com>
2022-05-12 20:19:03 +0900

    [FIX] body에 file url 추가

--- src/main/java/com/huitdduru/madduru/diary/service/DiaryService.java
+++ src/main/java/com/huitdduru/madduru/diary/service/DiaryService.java
@@ -3,14 +3,12 @@ package com.huitdduru.madduru.diary.service;
 import com.huitdduru.madduru.diary.payload.request.DiaryRequest;
 import com.huitdduru.madduru.diary.payload.response.DiaryListResponse;
 import com.huitdduru.madduru.diary.payload.response.DiaryDetailResponse;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface DiaryService {
 
-    void writeDiary(int diaryId, MultipartFile file, DiaryRequest diaryRequest) throws IOException;
+    void writeDiary(int diaryId, DiaryRequest diaryRequest);
 
     List<DiaryListResponse> choronology();
 

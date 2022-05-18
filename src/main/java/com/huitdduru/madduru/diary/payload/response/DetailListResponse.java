e077b4e9af138b8adbdbf212cc0b6e06718d6d1a
임서영 <sylim0318@naver.com>
2022-05-18 23:13:26 +0900

    [ADD] detail list response

---
+++ src/main/java/com/huitdduru/madduru/diary/payload/response/DetailListResponse.java
@@ -0,0 +1,19 @@
+package com.huitdduru.madduru.diary.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetailListResponse {
+
+    private String matchedUserName;
+    private List<DiaryDetailResponse> list;
+
+}

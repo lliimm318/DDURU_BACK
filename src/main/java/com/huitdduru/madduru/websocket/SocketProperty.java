7baacdfd7162693c292bd61e47b4c71ca9f03ac0
kwakdony <kwakdh25@gmail.com>
2022-06-07 12:06:11 +0900

    SocketProperty

--- src/main/java/com/huitdduru/madduru/websocket/SocketProperty.java
+++ src/main/java/com/huitdduru/madduru/websocket/SocketProperty.java
@@ -6,7 +6,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SocketProperty {
 
-    public static final String JOIN_KEY = "join";
+    public static final String USERINFO_KEY = "userinfo";
 
     public static final String ERROR_KEY = "error";
+
+    public static final String ACCEPT_KEY = "accept";
+
+    public static final String ROOM_ID_KEY = "room_id";
+
+    public static final String CANCEL_KEY = "cancel";
+
+    public static final String WAITING_KEY = "waiting";
 }

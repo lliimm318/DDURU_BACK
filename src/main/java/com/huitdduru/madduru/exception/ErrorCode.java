14afebaf498d2b191bff9970ffc593004e97c76d
kwakdony <kwakdh25@gmail.com>
2022-06-07 12:04:26 +0900

    MatchingException

--- src/main/java/com/huitdduru/madduru/exception/ErrorCode.java
+++ src/main/java/com/huitdduru/madduru/exception/ErrorCode.java
@@ -17,7 +17,8 @@ public enum ErrorCode {
     NOT_MAIL_FORMAT(500, "not email format"),
     USER_NOT_ACCESS(500, "user not access"),
     EMAIL_NOT_FORMAT(500, "not in email format"),
-    MAIL_SEND_ERROR(500, "email send error");
+    MAIL_SEND_ERROR(500, "email send error"),
+    MATCHING_EXCEPTION(500, "something wrong during matching");
 
     private final int status;
 

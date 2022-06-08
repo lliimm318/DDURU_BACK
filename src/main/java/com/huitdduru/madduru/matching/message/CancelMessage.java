ea7066dd288ca9b22e1d59565d9bcc5d2cdc89ab
kwakdony <kwakdh25@gmail.com>
2022-06-08 14:37:15 +0900

    websocket message

--- src/main/java/com/huitdduru/madduru/matching/message/CancelMessage.java
+++
@@ -1,8 +0,0 @@
-package com.huitdduru.madduru.matching.message;
-
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public class CancelMessage {
-    private final String message;
-}
---
+++ src/main/java/com/huitdduru/madduru/matching/message/SimpleMessage.java
@@ -0,0 +1,12 @@
+package com.huitdduru.madduru.matching.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SimpleMessage {
+    private String message;
+}

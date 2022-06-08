5d44eda4d6dc3833f47d2dcd9b7d978342c61d03
kwakdony <kwakdh25@gmail.com>
2022-06-08 14:33:34 +0900

    Property

---
+++ src/main/java/com/huitdduru/madduru/matching/service/AcceptProperty.java
@@ -0,0 +1,13 @@
+package com.huitdduru.madduru.matching.service;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AcceptProperty {
+    public static final String TRUE = "true";
+
+    public static final String FALSE = "false";
+
+    public static final String NULL = "null";
+}
--- src/main/java/com/huitdduru/madduru/websocket/security/AuthenticationProperty.java
+++
@@ -1,5 +0,0 @@
-package com.huitdduru.madduru.websocket.security;
-
-public class AuthenticationProperty {
-    public static final String USER_KEY = "userInfo";
-}
